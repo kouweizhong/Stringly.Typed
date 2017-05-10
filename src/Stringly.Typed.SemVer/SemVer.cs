481c25203356d945b220d52e245a174e264cc7cc
Rob Cooper <robc.the.geek@gmail.com>
2017-05-10 06:29:48 +0100

    Add ALL THE TODOS!

---
+++ src/Stringly.Typed.SemVer/SemVer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using StringlyTyped;
+
+namespace Stringly.Typed.SemVer
+{
+    public class SemVer : StringlyPattern<SemVer>
+    {
+        public int Major { get; private set; }
+
+        protected override Regex Regex => new Regex(@"\d+");
+        protected override SemVer ParseFromRegex(Match match)
+        {
+            return null;
+        }
+    }
+}
---
+++ tests/StringlyTyped.Tests/Types/SemVerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Stringly.Typed.SemVer;
+
+namespace StringlyTyped.Tests.Types
+{
+    public class SemVerTests
+    {
+        // http://semver.org/
+
+        [Test]
+        public void SemVer_EmptyString_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => { var sv = new Stringly<SemVer>(string.Empty); });
+        }
+
+        // TODO (RC): Major Non-Negative
+        // TODO (RC): Minor Non-Negative
+        // TODO (RC): Patch Non-Negative
+        // TODO (RC): Major Non-Leading Zeroes
+        // TODO (RC): Minor Non-Leading Zeroes
+        // TODO (RC): Patch Non-Leading Zeroes
+        // TODO (RC): Major v.0 == Unstable / Stable
+        // TODO (RC): Major > v.0 = !Unstable / Stable
+
+        // TODO (RC): SemVer + Patch = Bump Patch
+        // TODO (RC): SemVer + Minor = Bump Minor, Reset Patch
+        // TODO (RC): SemVer + Major = Bump Minor and Patch, Reset Minor and Patch
+
+        // TODO (RC): Pre-Release Version - "1.2.3-Alpha"
+        //  - Dot-Separated
+        //  - Non-Empty Identifiers
+        //  - Alphanumeric
+        //  - No Leading Zeroes in Numeric Identifiers
+        //  - Pre-Release < Normal Release
+        //  - "1.2.3-Staging" == IsPreRelease / !IsNormal
+        //  - "1.2.3-Staging" == Unstable / !Stable
+
+        // TODO (RC): Build Metadata - "1.2.3+321337
+        //  - Dot-Separated
+        //  - Non-Empty Identifiers
+        //  - Alphanumeric
+        //  - Ignored Determining Precedence
+        //  - Two Identical Versions w/ Different Build Metadata Are Equal
+
+        // TODO (RC): Precedence - http://semver.org/#spec-item-11
+
+        // TODO (RC): a.SemVer > b.SemVer - "New Version"
+        // TODO (RC): b.IsBreaking(a) - (b.Major > a.Major)
+
+
+        private void DoSemVerStuff(SemVer semVer)
+        {
+
+        }
+    }
+}
