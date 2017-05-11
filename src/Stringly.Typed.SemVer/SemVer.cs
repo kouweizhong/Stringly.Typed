29c5dc93b12ac21bf8d3b95133ec39bd2a706e6d
Rob Cooper <robc.the.geek@gmail.com>
2017-05-11 06:25:18 +0100

    Add PreRelease and Build Identifiers
    - :warning: Refactor Pending!

--- src/Stringly.Typed.SemVer/SemVer.cs
+++ src/Stringly.Typed.SemVer/SemVer.cs
@@ -1,21 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using StringlyTyped;
 
 namespace Stringly.Typed.SemVer
 {
     public class SemVer : StringlyPattern<SemVer>
     {
-        public int Major { get; private set; }
+        public uint Major { get; private set; }
+        public uint Minor { get; private set; }
+        public uint Patch { get; private set; }
+        public bool IsUnstable { get; private set; }
+        public bool IsStable { get; private set; }
+        public bool IsPreRelease { get; private set; }
+        public IList<string> PreReleaseIdentifiers { get; private set; }
+        public IList<string> BuildIdentifiers { get; private set; }
 
-        protected override Regex Regex => new Regex(@"\d+");
+        protected override Regex Regex => new Regex(@"^(?<Major>[1-9]+|0)\.(?<Minor>[1-9]+|0)\.(?<Patch>[1-9]+|0)(?:\-(?<PreRelease>(?:(?:[a-zA-Z]|[1-9]+)+\.?)+))?(?:\+(?<Build>(?:(?:[a-zA-Z0-9]+)+\.?)+))?$");
         protected override SemVer ParseFromRegex(Match match)
         {
-            return null;
+            var major = uint.Parse(match.Groups["Major"].Value);
+            var minor = uint.Parse(match.Groups["Minor"].Value);
+            var patch = uint.Parse(match.Groups["Patch"].Value);
+
+            var prValue = match.Groups["PreRelease"].Success
+                ? match.Groups["PreRelease"].Value
+                : string.Empty;
+            var pri = prValue.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var bValue = match.Groups["Build"].Success
+                ? match.Groups["Build"].Value
+                : string.Empty;
+            var bi = bValue.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            return new SemVer(major, minor, patch, pri, bi);
+        }
+
+        public SemVer()
+        {
+            // Default Paramless for Stringly.Typed
+        }
+
+        public SemVer(uint major, uint minor, uint patch, IList<string> prereleaseIdentifiers = null, IList<string> buildIdentifiers = null)
+        {
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+
+            IsStable = Major > 0;
+            IsUnstable = !IsStable;
+
+            if (prereleaseIdentifiers != null && prereleaseIdentifiers.Any())
+            {
+                PreReleaseIdentifiers = prereleaseIdentifiers.ToList();
+                IsPreRelease = true;
+                IsStable = false;
+                IsUnstable = true;
+            }
+            else
+            {
+                PreReleaseIdentifiers = new List<string>();
+            }
+
+            BuildIdentifiers = (prereleaseIdentifiers != null && prereleaseIdentifiers.Any())
+                ? buildIdentifiers.ToList()
+                : new List<string>();
         }
     }
 }
--- tests/StringlyTyped.Tests/Types/SemVerTests.cs
+++ tests/StringlyTyped.Tests/Types/SemVerTests.cs
@@ -15,44 +15,195 @@ namespace StringlyTyped.Tests.Types
                 () => { var sv = new Stringly<SemVer>(string.Empty); });
         }
 
-        // TODO (RC): Major Non-Negative
-        // TODO (RC): Minor Non-Negative
-        // TODO (RC): Patch Non-Negative
-        // TODO (RC): Major Non-Leading Zeroes
-        // TODO (RC): Minor Non-Leading Zeroes
-        // TODO (RC): Patch Non-Leading Zeroes
-        // TODO (RC): Major v.0 == Unstable / Stable
-        // TODO (RC): Major > v.0 = !Unstable / Stable
+        [Test]
+        public void SemVer_NormalSemVer_ReturnsResult()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3");
+
+            Assert.That(sv.Major, Is.EqualTo(1));
+            Assert.That(sv.Minor, Is.EqualTo(2));
+            Assert.That(sv.Patch, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void SemVer_NegativeMajor_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("-1.2.3");
+            });
+        }
+
+        [Test]
+        public void SemVer_NegativeMinor_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.-2.3");
+            });
+        }
+
+        [Test]
+        public void SemVer_NegativePatch_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.-3");
+            });
+        }
+
+        [Test]
+        public void SemVer_MajorLeadingZeroes_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("01.2.3");
+            });
+        }
+
+        [Test]
+        public void SemVer_MinorLeadingZeroes_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.02.3");
+            });
+        }
+
+        [Test]
+        public void SemVer_PatchLeadingZeroes_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("01.2.3");
+            });
+        }
+
+        [Test]
+        public void SemVer_MajorIsZero_MarkedUnstable()
+        {
+            SemVer sv = new Stringly<SemVer>("0.1.2");
+
+            Assert.That(sv.IsUnstable, Is.True);
+            Assert.That(sv.IsStable, Is.False);
+        }
+
+        [Test]
+        public void SemVer_MajorIsNonZero_MarkedStable()
+        {
+            SemVer sv = new Stringly<SemVer>("1.1.2");
+
+            Assert.That(sv.IsUnstable, Is.False);
+            Assert.That(sv.IsStable, Is.True);
+        }
+
+        [Test]
+        public void SemVer_SinglePreReleaseVersion_SetsPreRelease()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3-Alpha");
+
+            Assert.That(sv.IsPreRelease, Is.True);
+            Assert.That(sv.IsStable, Is.False);
+            Assert.That(sv.IsUnstable, Is.True);
+            Assert.That(sv.PreReleaseIdentifiers.Contains("Alpha"));
+        }
+
+        [Test]
+        public void SemVer_MultiplePreReleaseVersions_SetsPreRelease()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3-Alpha.Staging2.123");
+
+            Assert.That(sv.IsPreRelease, Is.True);
+            Assert.That(sv.IsStable, Is.False);
+            Assert.That(sv.IsUnstable, Is.True);
+            Assert.That(sv.PreReleaseIdentifiers.Contains("Alpha"));
+            Assert.That(sv.PreReleaseIdentifiers.Contains("Staging2"));
+            Assert.That(sv.PreReleaseIdentifiers.Contains("123"));
+        }
+
+        [Test]
+        public void SemVer_NonAlphaPreReleaseIdentifier_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.3-Staging!");
+            });
+        }
+
+        [Test]
+        public void SemVer_EmptyPreReleaseIdentifier_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.3-");
+            });
+        }
+
+        [Test]
+        public void SemVer_NumericPreReleaseIdentifierLeadingZeroes_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.3-01");
+            });
+        }
+
+        [Test]
+        public void SemVer_SingleBuildIdentifier_SetsBuild()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3+build");
+
+            Assert.That(sv.BuildIdentifiers.Contains("build"));
+        }
+
+        [Test]
+        public void SemVer_MultipleBuildIdentifiers_SetsBuild()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3+build.staging2.0123");
+
+            Assert.That(sv.BuildIdentifiers.Contains("build"));
+            Assert.That(sv.BuildIdentifiers.Contains("staging2"));
+            Assert.That(sv.BuildIdentifiers.Contains("0123"));
+        }
+
+        [Test]
+        public void SemVer_NonAlphaBuildIdentifier_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.3+Staging!");
+            });
+        }
+
+        [Test]
+        public void SemVer_EmptyBuildIdentifier_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                SemVer sv = new Stringly<SemVer>("1.2.3+");
+            });
+        }
+
+        [Test]
+        public void SemVer_PreReleaseAndBuildIdentifiers_SetsAllIdentifiers()
+        {
+            SemVer sv = new Stringly<SemVer>("1.2.3-Staging+build0123");
+
+            Assert.That(sv.PreReleaseIdentifiers.Contains("Staging"));
+            Assert.That(sv.BuildIdentifiers.Contains("build0123"));
+        }
+
+        // TODO (RC): Enable Implementers to override the Exception Message - make it helpful to provide the right format!
+
+        // TODO (RC): Precedence - http://semver.org/#spec-item-11
+
+        // Pre-Release < Normal Release
+        // Build Numbers Have No Meaning
+        // TODO (RC): a.SemVer > b.SemVer - "New Version"
+        // TODO (RC): b.IsBreaking(a) - (b.Major > a.Major)
 
         // TODO (RC): SemVer + Patch = Bump Patch
         // TODO (RC): SemVer + Minor = Bump Minor, Reset Patch
         // TODO (RC): SemVer + Major = Bump Minor and Patch, Reset Minor and Patch
-
-        // TODO (RC): Pre-Release Version - "1.2.3-Alpha"
-        //  - Dot-Separated
-        //  - Non-Empty Identifiers
-        //  - Alphanumeric
-        //  - No Leading Zeroes in Numeric Identifiers
-        //  - Pre-Release < Normal Release
-        //  - "1.2.3-Staging" == IsPreRelease / !IsNormal
-        //  - "1.2.3-Staging" == Unstable / !Stable
-
-        // TODO (RC): Build Metadata - "1.2.3+321337
-        //  - Dot-Separated
-        //  - Non-Empty Identifiers
-        //  - Alphanumeric
-        //  - Ignored Determining Precedence
-        //  - Two Identical Versions w/ Different Build Metadata Are Equal
-
-        // TODO (RC): Precedence - http://semver.org/#spec-item-11
-
-        // TODO (RC): a.SemVer > b.SemVer - "New Version"
-        // TODO (RC): b.IsBreaking(a) - (b.Major > a.Major)
-
-
-        private void DoSemVerStuff(SemVer semVer)
-        {
-
-        }
     }
 }
